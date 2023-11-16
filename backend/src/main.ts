3660b0dabaa4a77416062d20cabd32544118c51c
forato <lucas7forato@gmail.com>
2023-11-16 19:30:54 -0300

    feat: added config to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(5000, () => console.log('Server running on port 5000'));
+  await app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`),
+  );
 }
 bootstrap();
