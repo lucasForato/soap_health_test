9c17edce971e67ffd45c6c320445942c01e68f13
forato <lucas7forato@gmail.com>
2023-11-17 12:38:52 -0300

    feat: finished search

--- frontend/src/components/Search/index.tsx
+++ frontend/src/components/Search/index.tsx
@@ -29,9 +29,9 @@ const SearchBar: React.FC<Props> = ({ setListHasUpdated, setSearchParams }) => {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row w-10/12 lg:w-11/12 items-center h-40 justify-around">
+    <div className="flex flex-col lg:flex-row w-10/12 lg:w-11/12 items-center h-32 justify-around">
       <input
-        className="border-2 border-gray-300 bg-white h-10 px-5 lg:pr-16 w-2/3 rounded-l-md rounded-r-md lg:rounded-r-none border-l-none text-sm lg:w-2/5 focus:outline-none"
+        className="border-2 border-gray-300 bg-white h-10 px-5 lg:pr-16 rounded-md lg:w-2/5 w-full border-l-none text-sm focus:outline-none"
         type="search"
         name="name"
         placeholder="Name"
@@ -39,7 +39,7 @@ const SearchBar: React.FC<Props> = ({ setListHasUpdated, setSearchParams }) => {
         onChange={handleNameChange}
       />
       <input
-        className="border-2 border-gray-300 bg-white h-10 px-5 lg:pr-16 w-2/3 rounded-md lg:rounded-none text-sm lg:w-2/5 focus:outline-none"
+        className="border-2 border-gray-300 bg-white h-10 px-5 lg:pr-16 rounded-md lg:w-2/5 w-full text-sm focus:outline-none"
         type="tel"
         name="phone"
         placeholder="Phone Number"
